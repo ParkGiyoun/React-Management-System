c1cc7f6968c912f92dce3882f42a69f967585cc2
cosil2b2 <53854070+cosil2b2@users.noreply.github.com>
2020-08-06 21:27:37 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Customer from "./component/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "Hong",
+    birthday: "020115",
+    gender: "male",
+    job: "student",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any",
+    name: "기윤",
+    birthday: "020115",
+    gender: "male",
+    job: "student",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any",
+    name: "Park",
+    birthday: "020115",
+    gender: "male",
+    job: "student",
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customer.map((c) => {
+          const { id, image, name, birthday, gender, job } = c;
+          return (
+            <Customer
+              key={id}
+              id={id}
+              image={image}
+              name={name}
+              birthday={birthday}
+              gender={gender}
+              job={job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
